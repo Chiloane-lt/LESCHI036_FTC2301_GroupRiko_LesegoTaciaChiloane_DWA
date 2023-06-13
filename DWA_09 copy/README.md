375522f6bd3434389df4d294f3f6c51772d10002
Chiloane-lt <chiloane.lt@hotmail.com>
2023-06-13 11:09:46 +0200

    Update template.

---
+++ DWA_09 copy/README.md
@@ -0,0 +1,3 @@
+
+# LESCHI036_FTC2301_GroupRiko_LesegoTaciaChiloane_DWA
+Repository of projects for the DWA CodeSpace course.
---
+++ DWA_09 copy/scripts.js
@@ -0,0 +1,10 @@
+/* eslint-disable import/extensions */
+/* eslint-disable import/prefer-default-export */
+// @ts-check
+
+// import { initialPageLoad, createShowMoreButton, showMorePages } from './modules/page-loading.js';
+import './modules/summary.js';
+
+// initialPageLoad();
+// createShowMoreButton();
+// showMorePages();
